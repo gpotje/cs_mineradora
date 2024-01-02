122a80f5dc33e54534d03842d3c89450d04d78bb
Gabriel Potje de Souza <gabrielps@apoioprodesp.sp.gov.br>
2024-01-02 20:08:18 -0300

    MS report

---
+++ report/src/main/java/org/br/mineradora/utils/CSVHelper.java
@@ -0,0 +1,13 @@
+package org.br.mineradora.utils;
+
+import java.io.ByteArrayInputStream;
+
+public class CSVHelper {
+
+    public static ByteArrayInputStream OpportunitiesToCSV(){
+
+        return null;
+    }
+
+
+}
